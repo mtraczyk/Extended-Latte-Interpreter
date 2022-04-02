3cdcc9dfe29dc605212e55eca69943efc922ea3d
mtraczyk <michaltraczyk943@gmail.com>
2022-04-02 22:51:32 +0200

    Environment work in progress

--- src/Environment/Environment.hs
+++ src/Environment/Environment.hs
@@ -15,7 +15,16 @@ data Store = Store {vStore :: M.Map Location SimpleType,
   fStore :: M.Map Location FunctionType, vAlloc :: Int, fAlloc :: Int}
 
 data SimpleType = Int Integer | Str String | Bool Bool deriving (Eq, Show)
-data FunctionType = Fun [Arg] Block Env | Void
+data FunctionType = TFun [Arg] Block Env | TVoid
+
+getFunctionArgs :: FunctionType -> [Arg]
+getFunctionArgs (TFun args _ _) = args
+
+getFunctionBlock :: FunctionType -> Block
+getFunctionBlock (TFun _ block _) = block
+
+getFunEnv :: FunctionType -> Env
+getFunEnv (TFun _ _ env) = env
 
 getStoreSimpleType :: Location -> Store -> SimpleType
 getStoreSimpleType loc Store{..} = fromJust $ M.lookup loc vStore
@@ -42,6 +51,16 @@ putStoreFunctionType fun store = (fAlloc', store') where
   fAlloc' = (fAlloc store) + 1
   store' = Store {vStore = vStore store, fStore = M.insert fAlloc' fun (fStore store),
     vAlloc = vAlloc store, fAlloc = fAlloc'}
+
+getEnvLocation :: Ident -> Env -> Location
+getEnvLocation ident Env{..} = fromJust $ M.lookup ident env
+
+putEnvLocation :: Ident -> Location -> Env -> Env
+putEnvLocation ident loc Env{..} = Env {env = M.insert ident loc env}
+
+
+
+
 
 
 
